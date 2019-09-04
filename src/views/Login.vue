68eb230c282397092de896db5cea62251201874e
zuojianglong <3155481042@qq.com>
2019-09-05 00:33:12 +0800

    vee-validate使用验证规则

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,8 +5,23 @@
    <van-nav-bar title="登录"></van-nav-bar>
     <!-- 输入框 -->
     <van-cell-group>
-      <van-field v-model="user.mobile" required clearable left-icon="phone-o" placeholder="请输入手机号"></van-field>
-      <van-field v-model="user.code" required clearable left-icon="lock" placeholder="请输入验证码">
+      <van-field
+      v-validate="'required|digits:11'"
+      name="mobile"
+      :error-message="errors.first('mobile')"
+      v-model="user.mobile"
+      clearable
+      left-icon="phone-o"
+      placeholder="请输入手机号"></van-field>
+      <van-field
+      v-validate="'required|digits:11'"
+      name="code"
+      :error-message="errors.first('code')"
+      v-model="user.code"
+      required
+      clearable
+      left-icon="lock"
+      placeholder="请输入验证码">
         <van-button slot="button" type="primary" size="small">发送验证码</van-button>
       </van-field>
     </van-cell-group>
