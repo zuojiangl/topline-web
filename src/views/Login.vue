95259b6ae3ed60242129bfdd57107f3b71f43d9a
zuojianglong <3155481042@qq.com>
2019-09-04 16:32:01 +0800

    登录页面布局

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,15 +1,39 @@
 <template>
   <div>
-    <h2>登录页面</h2>
+    <!-- 导航头 -->
+
+   <van-nav-bar title="登录"></van-nav-bar>
+    <!-- 输入框 -->
+    <van-cell-group>
+      <van-field v-model="user.mobile" required clearable left-icon="phone-o" placeholder="请输入手机号"></van-field>
+      <van-field v-model="user.code" required clearable left-icon="lock" placeholder="请输入验证码">
+        <van-button slot="button" type="primary" size="small">发送验证码</van-button>
+      </van-field>
+    </van-cell-group>
+    <div class="login-btn">
+      <van-button class="van-btn" type="info">登录</van-button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      user: {
+        mobile: '13911111111',
+        code: '246810'
+      }
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.login-btn {
+  padding: 20px;
+  .van-btn {
+    width: 100%;
+  }
+}
 </style>
