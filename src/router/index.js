6b5252909d9a382f1a0e07423a09bb6de8af64ac
zuojianglong <3155481042@qq.com>
2019-09-05 08:20:29 +0800

    配置home的路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Tabbar from '@/views/Tabbar'
+import Home from '@/views/home/index.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    //   {
-    //   path: '/',
-    //   name: 'home',
-    //   component: Home
-    // },
-    {
-      path: '/login',
-      name: 'login',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
+  routes: [{
+    path: '/',
+    component: Tabbar,
+    children: [{
+      name: 'home',
+      path: '',
+      component: Home
+    }]
+  },
+  {
+    path: '/login',
+    name: 'login',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
                 import(/* webpackChunkName: "about" */ '../views/Login.vue')
-    }
+  }
   ]
 })
---
+++ src/views/Tabbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <router-view></router-view>
+    tabbar
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
