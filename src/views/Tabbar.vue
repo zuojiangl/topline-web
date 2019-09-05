5b904a2de7e7eb38d03d4d402abc571b1ba497c2
zuojianglong <3155481042@qq.com>
2019-09-05 08:29:09 +0800

    tabbar组件

--- src/views/Tabbar.vue
+++ src/views/Tabbar.vue
@@ -1,16 +1,24 @@
 <template>
   <div>
     <router-view></router-view>
-    tabbar
+    <van-tabbar route v-model="active">
+      <van-tabbar-item to="/" icon="home-o">首页</van-tabbar-item>
+      <van-tabbar-item to="qa" icon="search">问答</van-tabbar-item>
+      <van-tabbar-item to="video" icon="friends-o">视频</van-tabbar-item>
+      <van-tabbar-item to="/user" icon="setting-o">我的</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      active: 0
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
